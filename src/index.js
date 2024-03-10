c6e63f6add47154556380ae2b869f8644ee2b3f8
Yashashvi Jadaun <Yashashvi1004@gmail.com>
2024-03-11 02:14:59 +0530

    Slices functionality

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './Screens/HomeScreen';
 import ProductScreen from './Screens/ProductScreen';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,7 +29,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* <Provider store={store}> */}
     <RouterProvider router={router} />
+    {/* </Provider> */}
   </React.StrictMode>
 );
 
